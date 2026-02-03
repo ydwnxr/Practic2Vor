f6d9455207955f54b662188688db2916c5841037
tyuiuy <millkurichi1034@gmail.com>
2026-02-03 11:18:25 +0300

    Add files via upload

---
+++ Morozov291Practt2/Morozov291Practt2/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Morozov291Practt2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Manager.MainFrame = MainFrame;
+            Manager.MainFrame.Navigate(new LoginPage());
+        }
+    }
+}
